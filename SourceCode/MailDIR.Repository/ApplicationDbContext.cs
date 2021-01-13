072b6b48f816fe6c425c7532ad7b32534c984a40
suganthan-muthu <39992185+suganthan-muthu@users.noreply.github.com>
2021-01-13 08:40:31 +0530

    Add files via upload

---
+++ SourceCode/MailDIR.Repository/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using MailDIR.DataAccess;
+using Microsoft.EntityFrameworkCore;
+namespace MailDIR.Repository
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+                
+        public DbSet<User> Users { get; set; }
+        public DbSet<MailItem> MailItems { get; set; }
+        public DbSet<MailDetail> MailDetails { get; set; }
+        public DbSet<MailDetailsColMap> MailDetailsColMaps { get; set; }
+    }
+}
---
+++ SourceCode/MailDIR.Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailDIR.Repository
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();  
+        T GetByID(int id);
+        int Insert(T items);
+
+    }
+}
